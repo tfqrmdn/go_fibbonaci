3736ce17ea1eb8276453c68a589be57768bd0583
Taufiq Ramadhan <tfqrmdn@gmail.com>
2020-01-31 01:50:35 +0700

    add logic

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// fibonacci is a function that returns
+// a function that returns an int.
+func fibonacci() func() int {
+	x, y := 0, 1
+	return func() int {
+		res := x
+		x, y = y, x+y
+		return res
+	}
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin) // reader
+	fmt.Print("input: ")
+	input, _ := reader.ReadString('\n') // get input
+	input = strings.TrimSpace(input)    // remove \n from string
+	int, _ := strconv.Atoi(input)
+
+	f := fibonacci()
+	for i := 0; i < int; i++ {
+		fmt.Println(f())
+	}
+}

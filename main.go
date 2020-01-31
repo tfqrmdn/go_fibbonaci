6c23c33607ec8c0ac50519597f0ac6cade0b50ff
Taufiq Ramadhan <tfqrmdn@gmail.com>
2020-01-31 22:30:01 +0700

    format output

--- main.go
+++ main.go
@@ -8,13 +8,11 @@ import (
 	"strings"
 )
 
-// fibonacci is a function that returns
-// a function that returns an int.
 func fibonacci() func() int {
 	x, y := 0, 1
 	return func() int {
 		res := x
-		x, y = y, x+y
+		x, y = y, x+y // swap
 		return res
 	}
 }
@@ -24,7 +22,7 @@ func main() {
 	fmt.Print("input: ")
 	input, _ := reader.ReadString('\n') // get input
 	input = strings.TrimSpace(input)    // remove \n from string
-	int, _ := strconv.Atoi(input)
+	int, _ := strconv.Atoi(input)       // str to int
 
 	f := fibonacci()
 	for i := 0; i < int; i++ {
